e3f190c3a717f9c4ed0f723ec6b47f1e1088b794
wing <wlai@axway.com>
2020-06-19 11:16:57 +0100

    [RDAPI-20312] see if i can fix the publish directory problem

--- build.sh
+++ build.sh
@@ -58,22 +58,25 @@ function fMergeContent() {
 }
 
 function fRunHugo() {
-    cd ${PROJECT_DIR}
-    # this link is needed because netlify publish direcoty only works with public even when it's changed
-    #ln -s ${BUILD_DIR}/public public
     cd ${BUILD_DIR}
     mkdir public
     case "${MODE}" in
-        "dev") hugo server
+        "dev") 
+            hugo server
             ;;
-        "nelify") hugo 
+        "nelify") 
+            hugo
+            # Moving the "publish" directory to the ROOT of the workspace. Netlify can't publish a
+            # different directory even if the "Publish directory" is changed to specify a different directory.
+            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
             ;;
-        "nelify-preview") hugo -b $DEPLOY_PRIME_URL 
+        "nelify-preview") 
+            hugo -b $DEPLOY_PRIME_URL
+            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
             ;;
     esac
-    #ls -al ${PROJECT_DIR}/public
-    mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
 }
+
 fCheckout
 fMergeContent
 fRunHugo
